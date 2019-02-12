9a0fc29106f72fa05fa2268d0210fbcd7be645e2
cinguilherme <cinguilherme@gmail.com>
2019-02-12 16:42:32 -0200

    basic structure for the CLI to be tested

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,16 @@
-import { expect } from 'chai';
+const expect = require('chai').expect;
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+const exec = require('child_process').exec;
+
+const bitcoinConverter = 'node.exe ./src/main.js';
 
 describe('Main', () => {
-
+  it('should return string "Hello World!"', (done) => {
+    exec(bitcoinConverter, (err, stdout) => {
+      if (err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equals('Hello World!');
+      done();
+    });
+  });
 });
