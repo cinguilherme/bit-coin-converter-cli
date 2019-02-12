856f3aa02d872fdca80de8ada0c4199b4113a79c
cinguilherme <cinguilherme@gmail.com>
2019-02-12 17:54:41 -0200

    building on windows finally

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -3,13 +3,25 @@ const describe = require('mocha').describe;
 const it = require('mocha').it;
 const exec = require('child_process').exec;
 
-const bitcoinConverter = 'node.exe ./src/main.js';
+const isWin = process.platform === 'win32';
+const bitcoinConverter = isWin ? 'node.exe ./src/main.js' : './src/main.js';
+
+const pkg = require('../package.json');
 
 describe('Main', () => {
-  it('should return string "Hello World!"', (done) => {
-    exec(bitcoinConverter, (err, stdout) => {
+  it('should return string the version of the cli when --version', (done) => {
+    exec(`${bitcoinConverter} --version`, (err, stdout) => {
       if (err) throw err;
-      expect(stdout.replace('\n', '')).to.be.equals('Hello World!');
+      expect(stdout.replace('\n', '')).to.be.equals(pkg.version);
+      done();
+    });
+  });
+
+  it('should return string description of the cli when --help', (done) => {
+    exec(`${bitcoinConverter} --help`, (err, stdout) => {
+      if (err) throw err;
+      console.log(stdout);
+      expect(stdout.includes(pkg.description)).to.be.true;
       done();
     });
   });
